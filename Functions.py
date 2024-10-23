30f37f46d46967f40e3cf747fd5032cca5ee301c
LiviaFiebelkorn <70174011+LiviaFiebelkorn@users.noreply.github.com>
2024-10-23 11:44:37 -0500

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -29,3 +29,34 @@ def TIME_SERIES_DAILY_ADJUSTED(symbol):
     data = r.json()
 
     print(data)
+
+def TIME_SERIES_WEEKLY(symbol):
+
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol={symbol}&apikey={apikey}'
+    r = requests.get(url)
+    data = r.json()
+
+    print(data)
+
+def TIME_SERIES_WEEKLY_ADJUSTED(symbol):
+
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol={symbol}&apikey={apikey}'
+    r = requests.get(url)
+    data = r.json()
+
+    print(data)
+
+def TIME_SERIES_MONTHLY(symbol):
+
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={symbol}&apikey={apikey}'
+    r = requests.get(url)
+    data = r.json()
+
+    print(data)
+
+def TIME_SERIES_MONTHLY_ADJUSTED(symbol):
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol={symbol}&apikey={apikey}'
+    r = requests.get(url)
+    data = r.json()
+
+    print(data)
